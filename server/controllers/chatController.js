70b8c9bf30c49695d0c2a18ad165942da87fa76e
David Pina <davidpina14@gmail.com>
2018-01-10 22:34:33 -0600

    Added ability to create chat and message

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -1,21 +1,43 @@
+import { read } from "fs";
+
 var mongoose = require("mongoose");
 var Chat = mongoose.model("Chat");
 var Message = mongoose.model("Message");
 
 module.exports.create = function(req,res){
 
-    Chat.new({
-        participants:req.user._id, participants:req.reciever._id 
-    }
-        ,function(err,chat){
+    var chat = new Chat(
+        {
+            particapants: [ req.body.user, req.body.reciever ]
+        }
+    )
+
+    chat.save(function(err,newChat){
         if(err){
-            console.log(err);
+            console.log("Err")
         }
-        else{
-            console.log("Created new chat!")
-            console.log("***",chat)
-        }
-    });
+        var message = new Message({
+            chat: newChat._id,
+            user: req.body.FILLER,
+            message: req.body.message,
+        })
+
+        message.save(function(err,newMessage){
+            if(err){
+                console.log("error while making message")
+                console.log(err)
+            }
+            else{
+                Response.json({
+                    message:"Chst started",
+                    chatId: chat._id 
+                })
+            }
+        })
+    })
+
+
+
 }
 
 
